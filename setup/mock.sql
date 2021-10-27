d7c098f13fc6f5ad2dc07e658d0dcbaffc996714
Asadbek <89410739+asadbekGo@users.noreply.github.com>
2021-10-27 10:10:31 +0500

    Update mock.sql

--- setup/mock.sql
+++ setup/mock.sql
@@ -1,8 +1,9 @@
 
 insert into users(first_name, last_name, username, age) values
 ('Asadbek', 'Ergashev', 'asadbek001', 21),
-('Toirov', 'Botir', 'botir002', 20),
-('Rustamov', 'Sardor', 'sardor898', 17),
-('Odilov', 'Oybek', 'oybek02OK', 23),
-('Tolqinov', 'Samandar', 'saman@1101', 25)
+('Botir', 'Toirov', 'botir002', 20),
+('Sardor', 'Rustamov', 'sardor898', 17),
+('Oybek', 'Odilov', 'oybek02OK', 23),
+('Samandar','Tolqinov',  'saman@1101', 25)
 ;
+
